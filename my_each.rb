268e77e947e1f632542a37b9dabed3475ac32810
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-08 16:51:42 -0500

    Update my_each.rb

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
+members = ["tim", "lisa", "Scott"]
+
+def my_each(members)
+  counter = 0
+ while members.length < 10
+ yield members[i]
+  counter = counter + 1
 end
+return members 
+
+end 
+
+
+
+ 
+
+
+
+
+
+
