e6525b286022a8276bd99ecdf328b27edf2f96f5
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-09 01:11:49 -0500

    Update my_each.rb

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,16 @@
 members = ["tim", "lisa", "Scott"]
 
 def my_each(members)
+ if members = 0
+   return "need more members" 
   counter = 0
  while members.length < 10
  yield members[i]
   counter = counter + 1
-end
-return members 
-
-end 
-
+ end
+ members.each do |recruit|
+    puts "we love #{recruit}"
+ 
 
 
  
