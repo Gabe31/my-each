910033c429a95c08862c338d9e191d4c29130875
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-10-09 01:53:09 -0500

    Update my_each.rb

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,26 @@
+group = ["me", "you", "him","her"]
+
 def my_each(array)
   i = 0
   while i < array.length
     yield(array[i])
-    i = i = 1
+    i = i + 1
   end
 end 
 
-my_each(array) { |i| puts "this lab is killing me #{i}"}
+my_each(group) { |i| "this lab is killing me #{i} ughhhhh"}
 
+def hello(array)
+  i = 0
+  collection = []
+while i < array.length
+  collection << yield(array[i])
+  i += 1
+end
+collection
+end 
+
+hello(group) { |i| "is it over? #{i} let me know"}
 
 
 
